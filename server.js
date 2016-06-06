1fb938fa6629a869886a266bfbf033f572ab6540
Pietervh <pietvh@gmail.com>
2016-06-06 12:13:56 +0200

    Prep for Heroku

--- server.js
+++ server.js
@@ -3,9 +3,17 @@ var express = require('express');
 
 //create our app based on Express framework
 var app = express();
-
+const PORT = process.env.PORT || 3000;
 //Which folder to serve?
 //app.use() let's me define functionality for my application
+
+app.use(function(req,res,next){
+  if(req.headers['x-forwarded-proto']==='http'){
+    next();
+  }else{
+    res.redirect('http://'+req.hostname+req.url);
+  }
+});
 app.use(express.static('public'));
 
-app.listen(3000,function(){console.log("express server is up on 3000")});
+app.listen(PORT,function(){console.log("express server is up on 3000")});
